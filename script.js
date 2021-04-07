8b521552b4750f382c4ab68c8d06c9d4df5f42ad
Stephen Starbuck <62482847+stephenstarbuck@users.noreply.github.com>
2021-04-07 15:59:11 -0700

    Writing JS functions

--- script.js
+++ script.js
@@ -11,4 +11,26 @@ next.addEventListener("click", () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
+
+  update();
 });
+
+preview.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+}
